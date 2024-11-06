0d2d09a9b682bfa3fc7c6053529ea1ad3ffd8fd9
Kostya-star <konstantindanilov19@hotmail.com>
2024-11-06 20:56:42 +0200

    add tests for put request

--- src/mockDB/index.ts
+++ src/mockDB/index.ts
@@ -8,14 +8,14 @@ interface DataBase {
 export const mockDB: DataBase = {
   videos: [
     {
-      author: 'sds',
-      availableResolutions: ['P1080'],
-      canBeDownloaded: false,
-      createdAt: 'sds',
+      author: 'Author',
+      availableResolutions: ['P360'],
+      canBeDownloaded: true,
+      createdAt: '2011-10-05T14:48:00.100Z',
       id: 22,
-      minAgeRestriction: null,
-      publicationDate: '3434',
-      title: '222',
+      minAgeRestriction: 18,
+      publicationDate: '2011-10-05T14:48:00.100Z',
+      title: 'Title',
     },
   ],
 };
